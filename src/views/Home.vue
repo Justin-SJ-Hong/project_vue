68dc66e0b2d0d376d8a56da77274140f9df4b3bd
Walter-S-Hong <madwolves98@gmail.com>
2021-07-13 16:07:49 +0900

    home.vue 변경

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <div>배포테스트</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  //import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
 
-    components: {
-      HelloWorld,
-    },
+    // components: {
+    //   HelloWorld,
+    // },
   }
 </script>
