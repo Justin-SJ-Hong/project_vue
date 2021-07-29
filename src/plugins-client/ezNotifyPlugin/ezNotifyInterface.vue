a386737180361e1a2925682b285aadf56232f2e5
Justin-S-Hong <madwolves98@gmail.com>
2021-07-29 16:34:12 +0900

    플러그인

---
+++ src/plugins-client/ezNotifyPlugin/ezNotifyInterface.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ez-notify-component ref="notify" :opt="options" />
+  </div>
+</template>
+
+<script>
+import ezNotifyComponent from "./ezNotifyComponent.vue";
+export default {
+  components: { ezNotifyComponent },
+  name: "ezNotifyInterface",
+  methods: {
+    alert(content, title = "", option = {}) {
+      return this.$refs.notify.show(content, title, {
+        ...this.options,
+        ...option,
+        type: "alert",
+      });
+    },
+    confirm(content, title = "", option = {}) {
+      return this.$refs.notify.show(content, title, {
+        ...this.options,
+        ...option,
+        type: "confirm",
+      });
+    },
+    prompt(content, title = "", option = {}) {
+      return this.$refs.notify.show(content, title, {
+        ...this.options,
+        ...option,
+        type: "prompt",
+      });
+    },
+  },
+};
+</script>
