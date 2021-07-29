fb28a624f7f4e4481a930d41949284cdd16e12c3
Justin-S-Hong <madwolves98@gmail.com>
2021-07-29 23:00:20 +0900

    SSR 수정

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "../dist")));
 
 // Vue SSR
 const { createBundleRenderer } = require('vue-server-renderer');
-const template = fs.readFileSync(path.join(__dirname, "index.template.html"), "utf-8");
+const template = fs.readFileSync(path.join(__dirname, 'index.template.html'), 'utf-8');
 const serverBundle = require(path.join(__dirname, "../dist/vue-ssr-server-bundle.json"));
 const clientManifest = require(path.join(__dirname, "../dist/vue-ssr-client-manifest.json"));
 
